6944fbfebc29e649515ad88ab669a8841dcb4209
Yuri Machado <ymachado1995@gmail.com>
2024-05-03 21:46:51 -0300

    feat: general commit

--- resources/js/globalComponents.ts
+++
@@ -1,7 +0,0 @@
-// globalComponents.ts
-import { App as VueApp } from 'vue'
-import Message from './components/Message.vue'
-
-export function registerGlobalComponents(app: VueApp): void {
-    app.component('Message', Message)
-}
